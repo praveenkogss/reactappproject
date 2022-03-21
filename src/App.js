ad0496c5d3a7ee2e21444d049e7cb5d8d3311253
praveenkogss <ogss.praveenk@gmail.com>
2022-03-21 18:44:48 +0530

    xcdfsd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener norefdsadsaderrer"
+          rel="noopener"
         >
           Learn React
         </a>
