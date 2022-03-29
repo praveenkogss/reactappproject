659c2d3527f9ac3a33976135e234df9b3e15915d
praveenkogss <ogss.praveenk@gmail.com>
2022-03-29 11:34:44 +0530

    dv

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Core Runnning from Jenkins.
+          
         </p>
         <a
           className="App-link"
