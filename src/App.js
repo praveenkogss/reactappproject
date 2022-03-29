98f7ce7757ad9487303d9662b3eccc7650b7e416
praveenkogss <ogss.praveenk@gmail.com>
2022-03-29 12:03:59 +0530

    cvx

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Core Runnning from Jenkins.
-          
+          Runnning from Jenkins.
         </p>
         <a
           className="App-link"
